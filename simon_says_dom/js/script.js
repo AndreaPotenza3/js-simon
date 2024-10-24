8266ecb1c3db329e86203172c258eb1c3e00edf6
AndreaPotenza <andreapoten3@gmail.com>
2024-10-24 17:55:52 +0200

    progressi

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -1,36 +1,50 @@
 //Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
 //Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
 //Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
 const cells = document.getElementById("answers-form")
 const instruction = document.getElementById("instructions")
-const countdown = document.getElementById("countdown")
 const numbersList = document.getElementById("numbers-list")
-let countdownTimer = 6
+let countdown = document.getElementById("countdown")
+let countdownTimer = 30
 const randomNumbers = []
 
 // Generiamo 5 numeri casuali tra 1 - 50
 for (let i = 0; i < 5; i++) {
-    let number = Math.floor(Math.random(50, 1) * 50)
+    const number = Math.floor(Math.random(50, 1) * 50)
     randomNumbers.push(number)
 }
-
 console.log(randomNumbers)
 
+
+// Creiamo la lista di numeri
+
+for (let i = 0; i < randomNumbers.length; i++) {
+    let numbers = randomNumbers[i]
+
+    const liNumbers = document.createElement('li')
+    liNumbers.innerHTML = numbers
+    numbersList.append(liNumbers)
+}
+
 // Settiamo il countdown
-//setTimeout(timer, 5000)
+
+countdown.append(countdownTimer)
     
 const thirtySec =setInterval(countdownSetUp, 1000)
 
 // Funzioni
 
 function countdownSetUp(time){
+
     countdownTimer = --countdownTimer
 
     if (countdownTimer === 0) {
         clearInterval(thirtySec)
         cells.classList.remove('d-none')
-    instruction.innerHTML = 'Insersci i numeri visualizzati nella scheramata precendete!'
+    instruction.innerHTML = 'Insersci i numeri visualizzati nella schermata precendete!'
         
+    numbersList.classList.add('d-none')
     
     countdown.classList.add('d-none')
     }
